a24575b2313db9a78786e48080dde13533b80e95
Alex00Sam <alsa00@mail.ru>
2018-01-15 20:12:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,16 +3,16 @@ require 'vendor/autoload.php';
 
 use \atk4\ui\Header;
 
-$app = new \atk4\ui\App('Добро пожаловать в игру!');
+$app = new \atk4\ui\App('Welcome to the game!');
 $app->initLayout('Centered');
 
-$app->add(['Header', 'Инструкция']);
+$app->add(['Header', 'Instruction']);
 
 $text = $app->add(['Text']);
-$text->addParagraph('Эта игра демонстрирует, как использовать Links, Buttons, Headers, Text в Agile Toolkit.');
-$text->addParagraph("Для игры Вам необходимо загадать любое целое число от 1 до 100 включительно. После того, как Вы загадали число, нажмите кнопку 'Начать игру!' ");
+$text->addParagraph('Эта игра демонстрирует, как использовать Links, Buttons, Headers, Text в Agile Toolkit.This game demonstrates how to use links, buttons, headers and text in Agile Toolkit.');
+$text->addParagraph("You have to pick a number in interval 1-100. Next you press 'Start game!' button!");
 
-$button = $app->layout->add(['Button', "Начать игру!",'iconRight'=>'smile']);
+$button = $app->layout->add(['Button', "Start game!",'iconRight'=>'smile']);
 //$button->set(['primary'=>true]);
 $button->link(['main','max'=>100,'min'=>0]);
 $button->addClass('teal inverted');
