811eb0c9dcd610aa8ef63a798a601bdbdc7c1857
Alex00Sam <alsa00@mail.ru>
2018-01-15 20:13:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $app->initLayout('Centered');
 $app->add(['Header', 'Instruction']);
 
 $text = $app->add(['Text']);
-$text->addParagraph('Эта игра демонстрирует, как использовать Links, Buttons, Headers, Text в Agile Toolkit.This game demonstrates how to use links, buttons, headers and text in Agile Toolkit.');
+$text->addParagraph('This game demonstrates how to use links, buttons, headers and text in Agile Toolkit.');
 $text->addParagraph("You have to pick a number in interval 1-100. Next you press 'Start game!' button!");
 
 $button = $app->layout->add(['Button', "Start game!",'iconRight'=>'smile']);
