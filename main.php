24ab94d1055df69ac310c79ab5bce22bc7fd4b6b
Alex00Sam <alsa00@mail.ru>
2018-01-15 20:16:24 +0200

    Update main.php

--- main.php
+++ main.php
@@ -4,7 +4,7 @@ require 'vendor/autoload.php';
 
 use \atk4\ui\Header;
 
-$app = new \atk4\ui\App('Добро пожаловать в игру!');
+$app = new \atk4\ui\App('Welcome to the game!');
 $app->initLayout('Centered');
 
     $max = $_GET['max'];
@@ -12,17 +12,17 @@ $app->initLayout('Centered');
 
     $number = round(($max+$min)/2);
 
-    $app->add(new Header(['Ваше число это '.$number.' ?', 'size'=>1]));
+    $app->add(new Header(['Is your number '.$number.' ?', 'size'=>1]));
 
-    $button = $app->layout->add(['Button', "Да, это моё число!",'icon'=>'empty star']);
+    $button = $app->layout->add(['Button', "Yes, that's it!",'icon'=>'empty star']);
     $button->set(['primary'=>true]);
     $button->link(['win','number'=>$number]);
 
-    $button = $app->layout->add(['Button', 'Нет, моё число меньше.','icon'=>'arrow down']);
+    $button = $app->layout->add(['Button', "No, it's smaller.",'icon'=>'arrow down']);
     $button->link(['main','max'=>$number,'min'=>$min]);
 
-    $button = $app->layout->add(['Button', 'Нет, моё число больше.','icon'=>'arrow up']);
+    $button = $app->layout->add(['Button', "No, it's bigger.",'icon'=>'arrow up']);
     $button->link(['main','max'=>$max,'min'=>$number]);
 
-    $button = $app->layout->add(['Button', 'Сыграть ещё раз.','icon'=>'refresh']);
+    $button = $app->layout->add(['Button', 'Play again.','icon'=>'refresh']);
     $button->link(['index']);
